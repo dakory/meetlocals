bc8000506b6b4cba382f06f7cfbfaa3781a0561d
Софья Тимохина <sprh.t@MacBook-Pro-Sofa.local>
2020-11-08 15:06:37 +0300

    Удаление лишнего из модели

--- meetlocals/Modules/EventListModule/ViewModels/EventViewModel.swift
+++ meetlocals/Modules/EventListModule/ViewModels/EventViewModel.swift
@@ -18,7 +18,6 @@ struct EventViewModel {
     init(id: Int, idOrganizer: Int, name: String, description: String) {
         self.name = "Хочет \(name.lowercased())"
         self.id = id
-        self.name = name
         // TODO: прописать получение имени пользователя и аватарки из бд.
          self.userImage = "None"
          self.userName = "None"
